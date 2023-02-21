9c935e5300ced58bd5992373441e3c4b96adf5a5
wuxin <jay_dong@outlook.com>
2023-02-21 19:15:06 +0800

    fix:token校验

--- main.go
+++ main.go
@@ -1,20 +1,82 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"wxcloudrun-golang/db"
-	"wxcloudrun-golang/service"
+	"sort"
+	"strings"
 )
 
-func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
-	}
+const (
+	token = "sixah" //设置token
+)
 
-	http.HandleFunc("/", service.IndexHandler)
-	http.HandleFunc("/api/count", service.CounterHandler)
-
-	log.Fatal(http.ListenAndServe(":80", nil))
+func makeSignature(timestamp, nonce string) string { //本地计算signature
+	si := []string{token, timestamp, nonce}
+	sort.Strings(si)            //字典序排序
+	str := strings.Join(si, "") //组合字符串
+	s := sha1.New()             //返回一个新的使用SHA1校验的hash.Hash接口
+	io.WriteString(s, str)      //WriteString函数将字符串数组str中的内容写入到s中
+	return fmt.Sprintf("%x", s.Sum(nil))
 }
+
+func validateUrl(w http.ResponseWriter, r *http.Request) bool {
+	timestamp := strings.Join(r.Form["timestamp"], "")
+	nonce := strings.Join(r.Form["nonce"], "")
+	signature := strings.Join(r.Form["signature"], "")
+	echostr := strings.Join(r.Form["echostr"], "")
+	signatureGen := makeSignature(timestamp, nonce)
+
+	if signatureGen != signature {
+		return false
+	}
+	fmt.Fprintf(w, echostr) //原样返回eechostr给微信服务器
+	return true
+}
+
+func procSignature(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //Request需要解析
+	if !validateUrl(w, r) {
+		log.Println("Wechat Service: This http request is not from wechat platform")
+		return
+	}
+	log.Println("validateUrl Ok")
+}
+
+func main() {
+	log.Println("Wechat Service: Start!")
+	http.HandleFunc("/notifyMsg", procSignature)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Println("Wechat Service: ListenAndServe Error: ", err)
+	}
+	log.Println("Wechat Service: Stop!")
+}
+
+//func main() {
+//	if err := db.Init(); err != nil {
+//		panic(fmt.Sprintf("mysql init failed with %+v", err))
+//	}
+//	g := gin.Default()
+//	g.GET("/", func(c *gin.Context) {
+//
+//		signature := c.Query("signature")
+//		timestamp := c.Query("timestamp")
+//		nonce := c.Query("nonce")
+//		echostr := c.Query("echostr")
+//		token := "yuanshimima"
+//		if signature == "" || timestamp == "" || nonce == "" || echostr == "" {
+//			c.String(200, "参数错误")
+//			return
+//		}
+//
+//	})
+//
+//	//http.HandleFunc("/", service.IndexHandler)
+//	//http.HandleFunc("/api/count", service.CounterHandler)
+//	//
+//	//log.Fatal(http.ListenAndServe(":80", nil))
+//}
