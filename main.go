ab0d26b8024b3fd08db7afb8a0494de50f2b9673
black <68545150+sRGBXX@users.noreply.github.com>
2023-02-22 16:22:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -115,7 +115,7 @@ func procSignature(w http.ResponseWriter, r *http.Request) {
 
 	request, _ := http.NewRequest(http.MethodPost, "https://api.openai.com/v1/completions", strings.NewReader(string(bMsg)))
 	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Authorization", "Bearer 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")
+	request.Header.Set("Authorization", "Bearer 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")
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
 		log.Println(err)
